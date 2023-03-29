e7b3c4276ea5953f1296ab13a166e7c020e8ea69
Padmini <padmini@Badaris-MBP.modem>
2023-03-29 23:29:31 +1100

    Corrected the file path in app.rb

--- Mable/app.rb
+++ Mable/app.rb
@@ -1,9 +1,5 @@
-# require_relative 'app/models/customer_account_info'
-require_relative 'app/repositories/customer_account_repository'
 require_relative 'app/controllers/customer_account_controller'
 
-
-# repo = CustomerAccountRepository.new("data/mable_acc_balance.csv")
 company_name = "Alpha"
 repo = CompanyRepository.new("data/mable_acc_balance.csv", company_name)
 controller = CustomerAccountController.new("data/mable_trans.csv", repo)
